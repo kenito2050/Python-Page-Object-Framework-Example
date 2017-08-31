d1fa798063b7b2518413601c6df4a64c70ee02af
Ken Villarruel <kvillarruel@nasinsurance.com>
2017-08-31 14:57:11 -0700

    Checking in scripts.
    Updated CYB_TMLT script
    
    Updated the Before Sep 6 2017 Coverage Options classes.
    
    Confirmed scripts now run and are functional
    
    Also updated TMLT Excel worksheet (included more test scenarios)

--- pages/producer_center/saw/products/CYB_TMLT/coverage_options/Before_Sep_6_2017/No_PCI_coverage_options_Before_Sep_6_2017.py
+++ pages/producer_center/saw/products/CYB_TMLT/coverage_options/Before_Sep_6_2017/No_PCI_coverage_options_Before_Sep_6_2017.py
@@ -6,128 +6,48 @@ class No_PCI_Coverage_Options_Before_Sep_6_2017():
         self.driver = driver
 
     def PageElements(self):
-        # MEDEFENSE™ Plus Only
+
+        # Medefense™ Plus with $100,000 Disciplinary Proceedings Sublimit
 
         # Limits
         # 1MM/1MM
-        self.option_658_limit_2753 = self.driver.find_element(By.ID, "option-658-limit-2753")
-
-        # Deductibles
-        # $1,500
-        self.option_658_deductible_1186 = self.driver.find_element(By.ID, "option-658-deductible-1186")
-
-        # Cyber Liability Only - No PCI
-
-        # Limits
-        # 1MM/1MM
-        self.option_667_limit_2760 = self.driver.find_element(By.ID, "option-667-limit-2760")
+        self.option_479_limit_2136 = self.driver.find_element(By.ID, "option-479-limit-2136")
 
         # Deductibles
         # $0
-        self.option_667_deductible_1189 = self.driver.find_element(By.ID, "option-667-deductible-1189")
+        self.option_479_deductible_801 = self.driver.find_element(By.ID, "option-479-deductible-801")
 
-        # Cyber Liability with Breach Event Costs Outside the Limits - No PCI
-
-        # Limits
-        # 1MM/1MM
-        self.option_668_limit_2760 = self.driver.find_element(By.ID, "option-668-limit-2760")
-
-        # Deductibles
-        # $0
-        self.option_668_deductible_1189 = self.driver.find_element(By.ID, "option-668-deductible-1189")
-
-        # Cyber Liability with Claims Expenses Outside the Limits - No PCI
+        # TMLT Enhanced Cyber Liability with PCI and Cyber Crime Sublimit
 
         # Limits
         # 1MM/1MM
-        self.option_669_limit_2761 = self.driver.find_element(By.ID, "option-669-limit-2761")
+        self.option_481_limit_2139 = self.driver.find_element(By.ID, "option-481-limit-2139")
 
         # Deductibles
-        # $0
-        self.option_669_deductible_1189 = self.driver.find_element(By.ID, "option-669-deductible-1189")
+        # $0/$2,500
+        self.option_481_deductible_805 = self.driver.find_element(By.ID, "option-481-deductible-805")
 
-        # Cyber Liability with Claims Expenses Outside the Limits and with Breach Event Costs Outside the Limits - No PCI
+        # Medefense™ Plus with $100K Disciplinary Proceedings Sublimit and Enhanced Cyber Liability with PCI and Cyber Crime Sublimit
 
         # Limits
-        # 1MM/1MM
-        self.option_670_limit_2761 = self.driver.find_element(By.ID, "option-670-limit-2761")
+        # 1MM/2MM
+        self.option_483_limit_2141 = self.driver.find_element(By.ID, "option-483-limit-2141")
 
         # Deductibles
-        # $0
-        self.option_670_deductible_1189 = self.driver.find_element(By.ID, "option-670-deductible-1189")
-
-        # MEDEFENSE™ Plus and Cyber Liability Combined - No PCI
-
-        # Limits
-        # 1MM/1MM
-        self.option_671_limit_2762 = self.driver.find_element(By.ID, "option-671-limit-2762")
-
-        # Deductibles
-        # $1,500 / $0
-        self.option_671_deductible_1190 = self.driver.find_element(By.ID, "option-671-deductible-1190")
-
-        # MEDEFENSE™ Plus and Cyber Liability with Breach Event Costs Outside the Limits - No PCI
-
-        # Limits
-        # 1MM/1MM
-        self.option_672_limit_2762 = self.driver.find_element(By.ID, "option-672-limit-2762")
-
-        # Deductibles
-        # $1,500 / $0
-        self.option_672_deductible_1190 = self.driver.find_element(By.ID, "option-672-deductible-1190")
-
-        # MEDEFENSE™ Plus and Cyber Liability with Breach Event Costs Outside the Limits - No PCI
-
-        # Limits
-        # 1MM/1MM
-        self.option_673_limit_2763 = self.driver.find_element(By.ID, "option-673-limit-2763")
-
-        # Deductibles
-        # $1,500 / $0
-        self.option_673_deductible_1190 = self.driver.find_element(By.ID, "option-673-deductible-1190")
-
-        # MEDEFENSE™ Plus and Cyber Liability with Claims Expenses Outside the Limits - No PCI
-
-        # Limits
-        # 1MM/1MM
-        self.option_674_limit_2763 = self.driver.find_element(By.ID, "option-674-limit-2763")
-
-        # Deductibles
-        # $1,500 / $0
-        self.option_674_deductible_1190 = self.driver.find_element(By.ID, "option-674-deductible-1190")
+        # $0/$0/$2,500
+        self.option_483_deductible_807 = self.driver.find_element(By.ID, "option-483-deductible-807")
 
         return self
 
-    def select_Cyber_Liability_Only_No_PCI(self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_667_limit_2760.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_667_deductible_1189.click()
 
-    def select_Cyber_Liability_with_Breach_Event_Costs_Outside_the_Limits_No_PCI(self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_668_limit_2760.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_668_deductible_1189.click()
+    def select_Medefense_Plus_with_100K_Disciplinary_Proceedings_Sublimit_1M_Limit_0_Deduct(self):
+        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_479_limit_2136.click()
+        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_479_deductible_801.click()
 
-    def select_Cyber_Liability_with_Claims_Expenses_Outside_the_Limits_No_PCI(self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_669_limit_2761.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_669_deductible_1189.click()
+    def select_TMLT_Enhanced_Cyber_Liability_with_PCI_and_Cyber_Crime_Sublimit_1M_Limit_0_2pt5k_Deduct(self):
+        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_481_limit_2139.click()
+        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_481_deductible_805.click()
 
-    def select_Cyber_Liability_with_Claims_Expenses_Outside_the_Limits_and_with_Breach_Event_Costs_Outside_the_Limits_No_PCI(
-            self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_670_limit_2761.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_670_deductible_1189.click()
-
-    def select_MEDEFENSE_Plus_and_Cyber_Liability_Combined_No_PCI(self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_671_limit_2762.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_671_deductible_1190.click()
-
-    def select_MEDEFENSE_Plus_and_Cyber_Liability_with_Breach_Event_Costs_Outside_the_Limits_No_PCI(self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_672_limit_2762.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_672_deductible_1190.click()
-
-    def select_MEDEFENSE_Plus_and_Cyber_Liability_with_Claims_Expenses_Outside_the_Limits_No_PCI(self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_673_limit_2763.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_673_deductible_1190.click()
-
-    def select_MEDEFENSE_Plus_and_Cyber_Liability_with_Claims_Expenses_Outside_the_Limits_and_with_Breach_Event_Costs_Outside_the_Limits_No_PCI(
-            self):
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_674_limit_2763.click()
-        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_674_deductible_1190.click()
+    def select_Medefense_Plus_with_100K_Disciplinary_Proceedings_Sublimit_Enhanced_Cyber_Liability_with_PCI_and_Cyber_Crime_Sublimit_1M_Limit_0_0_2pt5k_Deduct(self):
+        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_483_limit_2141.click()
+        No_PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_483_deductible_807.click()
--- pages/producer_center/saw/products/CYB_TMLT/coverage_options/Before_Sep_6_2017/PCI_coverage_options_Before_Sep_6_2017.py
+++ pages/producer_center/saw/products/CYB_TMLT/coverage_options/Before_Sep_6_2017/PCI_coverage_options_Before_Sep_6_2017.py
@@ -7,182 +7,47 @@ class PCI_Coverage_Options_Before_Sep_6_2017():
 
     def PageElements(self):
 
-        # CyberRisk™ with Cyber Crime Only
+        # Medefense™ Plus with $100,000 Disciplinary Proceedings Sublimit
 
         # Limits
         # 1MM/1MM
-        self.option_435_limit_1920 = self.driver.find_element(By.ID, "option-435-limit-1920")
+        self.option_479_limit_2136 = self.driver.find_element(By.ID, "option-479-limit-2136")
 
         # Deductibles
-        # $50,000
-        self.option_435_deductible_772 = self.driver.find_element(By.ID, "option-435-deductible-772")
+        # $0
+        self.option_479_deductible_801 = self.driver.find_element(By.ID, "option-479-deductible-801")
 
-        # CyberRisk™ Only with Claim Expenses Outside Limits and Cyber Crime
+        # TMLT Enhanced Cyber Liability with PCI and Cyber Crime Sublimit
 
         # Limits
         # 1MM/1MM
-        self.option_436_limit_1942 = self.driver.find_element(By.ID, "option-436-limit-1942")
+        self.option_477_limit_2134 = self.driver.find_element(By.ID, "option-477-limit-2134")
 
         # Deductibles
-        # $25,000
-        self.option_436_deductible_772 = self.driver.find_element(By.ID, "option-436-deductible-772")
+        # $0/$2,500
+        self.option_477_deductible_802 = self.driver.find_element(By.ID, "option-477-deductible-802")
 
-        # Medefense™ Plus Only
+        # Medefense™ Plus with $100K Disciplinary Proceedings Sublimit and Enhanced Cyber Liability with PCI and Cyber Crime Sublimit
 
         # Limits
         # 1MM/2MM
-        self.option_309_limit_1523 = self.driver.find_element(By.ID, "option-309-limit-1523")
+        self.option_478_limit_2135 = self.driver.find_element(By.ID, "option-478-limit-2135")
 
         # Deductibles
-        # $25,000
-        self.option_309_deductible_54 = self.driver.find_element(By.ID, "option-309-deductible-54")
-
-        # Medefense™ Plus with Peer Review Proceeding Sublimit
-
-        # Limits
-        # 1MM/5MM
-        self.option_310_limit_1528 = self.driver.find_element(By.ID, "option-310-limit-1528")
-
-        # Deductibles
-        # $25,000
-        self.option_310_deductible_195 = self.driver.find_element(By.ID, "option-310-deductible-195")
-
-        # Medefense™ Plus and CyberRisk™ with Cyber Crime Combined Shared Limits
-
-        # Limits
-        # 1MM/1MM
-        self.option_431_limit_1923 = self.driver.find_element(By.ID, "option-431-limit-1923")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_431_deductible_773 = self.driver.find_element(By.ID, "option-431-deductible-773")
-
-        # Medefense™ Plus and CyberRisk™ Combined Shared Limits with Claim Expenses Outside Limits and Cyber Crime
-
-        # Limits
-        # 1MM/1MM
-        self.option_437_limit_1945 = self.driver.find_element(By.ID, "option-437-limit-1945")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_437_deductible_773 = self.driver.find_element(By.ID, "option-437-deductible-773")
-
-        # Medefense™ Plus and CyberRisk™ Combined Separate Limits with Cyber Crime
-
-        # Limits
-        # 1MM/5MM
-        self.option_432_limit_1929 = self.driver.find_element(By.ID, "option-432-limit-1929")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_432_deductible_773 = self.driver.find_element(By.ID, "option-432-deductible-773")
-
-        # Medefense™ Plus and CyberRisk™ Combined Separate Limits with Claim Expenses Outside Limits and Cyber Crime
-
-        # Limits
-        # 1MM/5MM
-        self.option_438_limit_1952 = self.driver.find_element(By.ID, "option-438-limit-1952")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_438_deductible_773 = self.driver.find_element(By.ID, "option-438-deductible-773")
-
-        # Medefense™ Plus with Peer Review Proceeding Sublimit and CyberRisk™ Combined Shared Limits with Cyber Crime
-
-        # Limits
-        # 1MM/1MM
-        self.option_433_limit_1932 = self.driver.find_element(By.ID, "option-433-limit-1932")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_433_deductible_759 = self.driver.find_element(By.ID, "option-433-deductible-759")
-
-        # Medefense™ Plus with Peer Review Proceeding Sublimit and CyberRisk™ Combined Shared Limits with Claim Expenses Outside Limits and Cyber Crime
-
-        # Limits
-        # 1MM/1MM
-        self.option_439_limit_1955 = self.driver.find_element(By.ID, "option-439-limit-1955")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_439_deductible_759 = self.driver.find_element(By.ID, "option-439-deductible-759")
-
-        # Medefense™ Plus with Peer Review Proceeding Sublimit and CyberRisk™ Combined Separate Limits with Cyber Crime
-
-        # Limits
-        # 1MM/5MM
-        self.option_434_limit_1939 = self.driver.find_element(By.ID, "option-434-limit-1939")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_434_deductible_759 = self.driver.find_element(By.ID, "option-434-deductible-759")
-
-        # Medefense™ Plus with Peer Review Proceeding Sublimit and CyberRisk™ Combined Separate Limits with Claim Expenses Outside Limits and Cyber Crime
-
-        # Limits
-        # 1MM/5MM
-        self.option_440_limit_1962 = self.driver.find_element(By.ID, "option-440-limit-1962")
-
-        # Deductibles
-        # $25K/$50K
-        self.option_440_deductible_759 = self.driver.find_element(By.ID, "option-440-deductible-759")
-
+        # $0/$0/$2,500
+        self.option_478_deductible_803 = self.driver.find_element(By.ID, "option-478-deductible-803")
 
         return self
 
-    # select_CyberRisk_with_Cyber_Crime_Only()
-    # select_CyberRisk_Only_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime()
-    # select_Medefense_Plus_Only()
-    # select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit()
-    # select_Medefense_Plus_and_CyberRisk_with_Cyber_Crime_Combined_Shared_Limits()
-    # select_Medefense_Plus_and_CyberRisk_Combined_Shared_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime()
-    # select_Medefense_Plus_and_CyberRisk_Combined_Separate_Limits_with_Cyber_Crime()
-    # select_Medefense_Plus_and_CyberRisk_Combined_Separate_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime()
-    # select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit_and_CyberRisk_Combined_Shared_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime
-    # select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit_and_CyberRisk_Combined_Shared_Limits_with_Cyber_Crime()
-    # select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit_and_CyberRisk_Combined_Separate_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime()
 
+    def select_Medefense_Plus_with_100K_Disciplinary_Proceedings_Sublimit_1M_Limit_0_Deduct(self):
+        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_479_limit_2136.click()
+        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_479_deductible_801.click()
 
-    def select_CyberRisk_with_Cyber_Crime_Only(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_435_limit_1920.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_435_deductible_772.click()
+    def select_TMLT_Enhanced_Cyber_Liability_with_PCI_and_Cyber_Crime_Sublimit_1M_Limit_0_2pt5k_Deduct(self):
+        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_477_limit_2134.click()
+        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_477_deductible_802.click()
 
-    def select_CyberRisk_Only_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_436_limit_1942.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_436_deductible_772.click()
-
-    def select_Medefense_Plus_Only(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_309_limit_1523.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_309_deductible_54.click()
-
-    def select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_310_limit_1528.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_310_deductible_195.click()
-
-    def select_Medefense_Plus_and_CyberRisk_with_Cyber_Crime_Combined_Shared_Limits(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_431_limit_1923.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_431_deductible_773.click()
-
-    def select_Medefense_Plus_and_CyberRisk_Combined_Shared_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_437_limit_1945.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_437_deductible_773.click()
-
-    def select_Medefense_Plus_and_CyberRisk_Combined_Separate_Limits_with_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_432_limit_1929.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_432_deductible_773.click()
-
-    def select_Medefense_Plus_and_CyberRisk_Combined_Separate_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_438_limit_1952.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_438_deductible_773.click()
-
-    def select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit_and_CyberRisk_Combined_Shared_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_439_limit_1955.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_439_deductible_759.click()
-
-    def select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit_and_CyberRisk_Combined_Shared_Limits_with_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_434_limit_1939.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_434_deductible_759.click()
-
-    def select_Medefense_Plus_with_Peer_Review_Proceeding_Sublimit_and_CyberRisk_Combined_Separate_Limits_with_Claim_Expenses_Outside_Limits_and_Cyber_Crime(self):
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_440_limit_1962.click()
-        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_440_deductible_759.click()
+    def select_Medefense_Plus_with_100K_Disciplinary_Proceedings_Sublimit_Enhanced_Cyber_Liability_with_PCI_and_Cyber_Crime_Sublimit_1M_Limit_0_0_2pt5k_Deduct(self):
+        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_478_limit_2135.click()
+        PCI_Coverage_Options_Before_Sep_6_2017.PageElements(self).option_478_deductible_803.click()
