2e9483ba3b4fee1d75eb7bd36ca4379c4bb4202c
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-05-25 14:57:00 -0700

    Checking in scripts.
    Checking in Updated Environments.py and Environments.xml files

--- utilities/Environments/Environments.py
+++ utilities/Environments/Environments.py
@@ -14,15 +14,18 @@ class Environments(dict):
             'Demo7': 'https://svcdemo7.wn.nasinsurance.com/',
             'Demo8': 'https://svcdemo8.wn.nasinsurance.com/',
             'Demo9': 'https://svcdemo9.wn.nasinsurance.com/',
+            'OS_Upgrade': 'https://svcstg.wn.nasinsurance.com/',
             'Fix': 'https://svcfix.wn.nasinsurance.com/',
             'Stage': 'https://svcrel.wn.nasinsurance.com/',
             'QA': 'https://svcqa.wn.nasinsurance.com/',
+            'Cust_Live': 'https://customer.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_Dev': 'https://custdev.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_Demo3': 'https://custdemo3.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_Demo4': 'https://custdemo4.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_Demo6': 'https://custdemo6.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_QA': 'https://custqa.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_Fix': 'https://custfix.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
+            'Retail_Access_Live': 'https://producer.wn.nasinsurance.com/registration',
             'Retail_Access_Demo7': 'https://proddemo7.wn.nasinsurance.com/registration',
             'Live': 'https://service.wn.nasinsurance.com/'
             }
