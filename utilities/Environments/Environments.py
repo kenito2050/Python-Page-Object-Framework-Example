0b67637c0db4847a3f192f36052f402b2f034d78
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-07-02 11:33:43 -0700

    Updated Environments.py
    SVCSTG now called Stage
    
    svcrel now called Old_Stage

--- utilities/Environments/Environments.py
+++ utilities/Environments/Environments.py
@@ -14,9 +14,9 @@ class Environments(dict):
             'Demo7': 'https://svcdemo7.wn.nasinsurance.com/',
             'Demo8': 'https://svcdemo8.wn.nasinsurance.com/',
             'Demo9': 'https://svcdemo9.wn.nasinsurance.com/',
-            'OS_Upgrade': 'https://svcstg.wn.nasinsurance.com/',
+            'Stage': 'https://svcstg.wn.nasinsurance.com/',
             'Fix': 'https://svcfix.wn.nasinsurance.com/',
-            'Stage': 'https://svcrel.wn.nasinsurance.com/',
+            'Old_Stage': 'https://svcrel.wn.nasinsurance.com/',
             'QA': 'https://svcqa.wn.nasinsurance.com/',
             'Cust_Live': 'https://customer.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_Dev': 'https://custdev.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
@@ -28,7 +28,7 @@ class Environments(dict):
             'Cust_Stage': 'https://custrel.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Cust_OS_Upgrade': 'https://custstg.wn.nasinsurance.com/index.php?c=program.welcome&id=19',
             'Retail_Access_Live': 'https://producer.wn.nasinsurance.com/registration',
-            'Retail_Access_Stage': 'https://prodrel.wn.nasinsurance.com/registration',
+            'Retail_Access_Stage': 'https://prodstg.wn.nasinsurance.com/registration',
             'Retail_Access_Demo7': 'https://proddemo7.wn.nasinsurance.com/registration',
             'Live': 'https://service.wn.nasinsurance.com/'
             }
