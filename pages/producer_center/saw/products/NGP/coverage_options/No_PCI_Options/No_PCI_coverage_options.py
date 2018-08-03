23c6a0c836d1a405a3fbb38138ba8735fddbca7c
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-08-03 13:58:21 -0700

    Checking in scripts.
    NGP No PCI Smoke Test Script

--- pages/producer_center/saw/products/NGP/coverage_options/No_PCI_Options/No_PCI_coverage_options.py
+++ pages/producer_center/saw/products/NGP/coverage_options/No_PCI_Options/No_PCI_coverage_options.py
@@ -14,35 +14,215 @@ class No_PCI_Coverage_Options():
         # 250K
         self.option_972_limit_3716 = self.driver.find_element(By.ID, "option-972-limit-3716")
 
+        # Label
+        self.label_option_972_limit_3716 = self.driver.find_element(By.XPATH, "//label[@for='option-972-limit-3716']")
+
         # 500K
         self.option_972_limit_3713 = self.driver.find_element(By.ID, "option-972-limit-3713")
+
+        # Label
+        self.label_option_972_limit_3713 = self.driver.find_element(By.XPATH, "//label[@for='option-972-limit-3713']")
 
         # 1MM
         self.option_972_limit_3714 = self.driver.find_element(By.ID, "option-972-limit-3714")
 
+        # Label
+        self.label_option_972_limit_3714 = self.driver.find_element(By.XPATH, "//label[@for='option-972-limit-3714']")
+
         # 2MM
         self.option_972_limit_3715 = self.driver.find_element(By.ID, "option-972-limit-3715")
+
+        # Label
+        self.label_option_972_limit_3715 = self.driver.find_element(By.XPATH, "//label[@for='option-972-limit-3715']")
 
         # Deductibles
         # $500
         self.option_972_deductible_1439 = self.driver.find_element(By.ID, "option-972-deductible-1439")
 
+        # Label
+        self.label_option_972_deductible_1439 = self.driver.find_element(By.XPATH, "//label[@for='option-972-deductible-1439']")
+
         # $1K
         self.option_972_deductible_1434 = self.driver.find_element(By.ID, "option-972-deductible-1434")
+
+        # Label
+        self.label_option_972_deductible_1434 = self.driver.find_element(By.XPATH, "//label[@for='option-972-deductible-1434']")
 
         # $2pt5K
         self.option_972_deductible_1435 = self.driver.find_element(By.ID, "option-972-deductible-1435")
 
+        # Label
+        self.label_option_972_deductible_1435 = self.driver.find_element(By.XPATH, "//label[@for='option-972-deductible-1435']")
+
         # $5K
         self.option_972_deductible_1436 = self.driver.find_element(By.ID, "option-972-deductible-1436")
+
+        # Label
+        self.label_option_972_deductible_1436 = self.driver.find_element(By.XPATH, "//label[@for='option-972-deductible-1436']")
 
         # $10K
         self.option_972_deductible_1437 = self.driver.find_element(By.ID, "option-972-deductible-1437")
 
+        # Label
+        self.label_option_972_deductible_1437 = self.driver.find_element(By.XPATH, "//label[@for='option-972-deductible-1437']")
+
         # $25K
         self.option_972_deductible_1438 = self.driver.find_element(By.ID, "option-972-deductible-1438")
 
+        # Label
+        self.label_option_972_deductible_1438 = self.driver.find_element(By.XPATH, "//label[@for='option-972-deductible-1438']")
+
+        # NetGuard™ Plus with BrandGuard™ Without PCI Per Identity
+
+        # Limits
+
+        # 250K
+        self.option_1013_limit_3823 = self.driver.find_element(By.ID, "option-1013-limit-3823")
+
+        # Label
+        self.label_option_1013_limit_3823 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-limit-3823']")
+
+        # 500K
+        self.option_1013_limit_3820 = self.driver.find_element(By.ID, "option-1013-limit-3820")
+
+        # Label
+        self.label_option_1013_limit_3820 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-limit-3820']")
+
+        # 1MM
+        self.option_1013_limit_3821 = self.driver.find_element(By.ID, "option-1013-limit-3821")
+
+        # Label
+        self.label_option_1013_limit_3821 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-limit-3821']")
+
+        # 2MM
+        self.option_1013_limit_3822 = self.driver.find_element(By.ID, "option-1013-limit-3822")
+
+        # Label
+        self.label_option_1013_limit_3822 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-limit-3822']")
+
+        # Deductibles
+
+        # $500
+        self.option_1013_deductible_1510 = self.driver.find_element(By.ID, "option-1013-deductible-1510")
+
+        # Label
+        self.label_option_1013_deductible_1510 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-deductible-1510']")
+
+        # $1K
+        self.option_1013_deductible_1505 = self.driver.find_element(By.ID, "option-1013-deductible-1505")
+
+        # Label
+        self.label_option_1013_deductible_1505 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-deductible-1505']")
+
+        # $2pt5K
+        self.option_1013_deductible_1506 = self.driver.find_element(By.ID, "option-1013-deductible-1506")
+
+        # Label
+        self.label_option_1013_deductible_1506 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-deductible-1506']")
+
+        # $5K
+        self.option_1013_deductible_1507 = self.driver.find_element(By.ID, "option-1013-deductible-1507")
+
+        # Label
+        self.label_option_1013_deductible_1507 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-deductible-1507']")
+
+        # $10K
+        self.option_1013_deductible_1508 = self.driver.find_element(By.ID, "option-1013-deductible-1508")
+
+        # Label
+        self.label_option_1013_deductible_1508 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-deductible-1508']")
+
+        # $25K
+        self.option_1013_deductible_1509 = self.driver.find_element(By.ID, "option-1013-deductible-1509")
+
+        # Label
+        self.label_option_1013_deductible_1509 = self.driver.find_element(By.XPATH, "//label[@for='option-1013-deductible-1509']")
+
         return self
+
+    ## Return Text for Limits
+
+    def return_NGP_with_BrandGuard_without_PCI_250K_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_250K_limit_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_limit_3716.text)
+        return NGP_with_BrandGuard_without_PCI_250K_limit_text
+
+    def return_NGP_with_BrandGuard_without_PCI_500K_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_500K_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_limit_3713.text)
+        return NGP_with_BrandGuard_without_PCI_500K_limit_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_1MM_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_1MM_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_limit_3714.text)
+        return NGP_with_BrandGuard_without_PCI_1MM_limit_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_2MM_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_2MM_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_limit_3715.text)
+        return NGP_with_BrandGuard_without_PCI_2MM_limit_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_250K_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_250K_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_limit_3823.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_250K_limit_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_500K_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_500K_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_limit_3820.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_500K_limit_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_1MM_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_1MM_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_limit_3821.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_1MM_limit_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_2MM_limit_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_2MM_limit_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_limit_3822.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_2MM_limit_label_text
+
+    ## Return Text for Deductibles
+
+    def return_NGP_with_BrandGuard_without_PCI_500_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_500_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_deductible_1439.text)
+        return NGP_with_BrandGuard_without_PCI_500_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_1k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_1k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_deductible_1434.text)
+        return NGP_with_BrandGuard_without_PCI_1k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_2pt5k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_2pt5k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_deductible_1435.text)
+        return NGP_with_BrandGuard_without_PCI_2pt5k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_5k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_5k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_deductible_1436.text)
+        return NGP_with_BrandGuard_without_PCI_5k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_10k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_10k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_deductible_1437.text)
+        return NGP_with_BrandGuard_without_PCI_10k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_25k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_25k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_972_deductible_1438.text)
+        return NGP_with_BrandGuard_without_PCI_25k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_500_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_500_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_deductible_1510.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_500_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_1k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_1k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_deductible_1505.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_1k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_2pt5k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_2pt5k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_deductible_1506.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_2pt5k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_5k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_5k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_deductible_1507.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_5k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_10k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_10k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_deductible_1508.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_10k_deductible_label_text
+
+    def return_NGP_with_BrandGuard_without_PCI_Per_Identity_25k_deductible_label_text(self):
+        NGP_with_BrandGuard_without_PCI_Per_Identity_25k_deductible_label_text = (No_PCI_Coverage_Options.PageElements(self).label_option_1013_deductible_1509.text)
+        return NGP_with_BrandGuard_without_PCI_Per_Identity_25k_deductible_label_text
 
     def select_NGP_with_BrandGuard_No_PCI_1MM_limit_2pt5K_Deduct(self):
         No_PCI_Coverage_Options.PageElements(self).option_972_limit_3714.click()
