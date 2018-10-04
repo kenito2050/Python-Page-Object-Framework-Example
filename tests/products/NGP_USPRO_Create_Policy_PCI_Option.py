0371bb54c4d27fa93387cfd29a86fc41378cdf99
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-03 17:26:50 -0700

    Checking in latest updates for NASONLINE Scripts
    Modified USPRO (Service Center / Producer Center) Scripts so that they will run unattended.
    
    Not able to run headless yet.
    
    Script fails at "Submit Subjectivities" step (in headless mode)

--- tests/products/NGP_USPRO_Create_Policy_PCI_Option.py
+++ tests/products/NGP_USPRO_Create_Policy_PCI_Option.py
@@ -1,8 +1,10 @@
 import unittest
+import datetime
 import os
 from urllib.parse import urlparse, parse_qs
 from xml.etree import ElementTree as ET
 
+import xlrd
 from faker import address
 from faker import company
 from faker import name
@@ -17,8 +19,8 @@ from pages.producer_center.client_contact_page import ClientContact
 from pages.producer_center.saw.coverage_periods_page import CoveragePeriods
 from pages.producer_center.saw.products.NGP_USPRO.insured_information.insured_information import Insured_Information
 from pages.producer_center.saw.products.NGP_USPRO.PAF.PAF import PAF
-from pages.producer_center.saw.products.NGP_USPRO.coverage_options.PCI_coverage_options import PCI_Coverage_Options
-from pages.producer_center.saw.products.NGP_USPRO.coverage_options.No_PCI_coverage_options import No_PCI_Coverage_Options
+from pages.producer_center.saw.products.NGP_USPRO.coverage_options.PCI_Options.PCI_coverage_options import PCI_Coverage_Options
+from pages.producer_center.saw.products.NGP_USPRO.coverage_options.No_PCI_Options.No_PCI_coverage_options import No_PCI_Coverage_Options
 from pages.producer_center.saw.products.NGP_USPRO.coverage_options.coverage_options import Coverage_Options
 from pages.producer_center.saw.products.NGP_USPRO.select_option.select_option import Select_Option
 from pages.producer_center.saw.quote_review import Quote_Review
@@ -41,30 +43,74 @@ from utilities.Environments.Environments import Environments
 from utilities.contract_classes.contract_classes import ContractClasses
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes_state_capitals.zip_codes import ZipCodes
+from config_globals import *
 
+class TestCreateQuote():
 
-class CreateQuote():
+    def test_login_search_for_agent_create_quote(self, browser, env):
 
-    def test_login_search_for_agent_create_quote(self):
+        Product = "NGP_USPRO"
+        driver = browser
 
         ## Directory Locations
 
-        tests_directory = os.path.abspath(os.pardir)
-        framework_directory = os.path.abspath(os.path.join(tests_directory, os.pardir))
-        config_file_directory = os.path.abspath(os.path.join(framework_directory, 'config_files'))
-        test_case_directory = os.path.abspath(os.path.join(framework_directory, 'utilities\Excel_Sheets\Products'))
-        test_results_directory = os.path.abspath(
-            os.path.join(framework_directory, 'utilities\Excel_Sheets\Test_Results'))
+        tests_directory = ROOT_DIR / 'tests'
+        framework_directory = ROOT_DIR
+        config_file_directory = CONFIG_PATH
+        test_case_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Products'
+        test_results_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Test_Results'
 
-        ## Determine Test Environment to run scripts
+        global test_summary
+        global test_scenario
+        global effective_date
+        global contract_class
+        global agent
+        global state
+        global revenue
+        global total_num_records
+        global _OLD_scenario
 
-        ## Read in value from test_environment.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
-        test_environment  = tree.getroot()
-        environment =(test_environment[0][0].text)
+        # Open Test Scenario Workbook; Instantiate worksheet object
+        # 0 - First Worksheet
+        # 1 - Second Worksheet...etc
+
+        wb = xlrd.open_workbook(str(test_case_directory / Product) + '.xlsx')
+        sh = wb.sheet_by_index(2)
+
+        ## Begin For Loop to iterate through Test Scenarios
+        i = 1
+        rows = sh.nrows
+        empty_cell = False
+        for x in range(1, sh.nrows):
+
+            cell_val = sh.cell(i, 0).value
+            if cell_val == '':
+                # If Cell Value is empty, set empty_cell to True
+                empty_cell = True
+            else:
+                # If Cell Value is NOT empty, set empty_cell to False
+                empty_cell = False
+
+            # Check to see if cell is NOT empty
+            # If cell is not empty, read in the values
+            if empty_cell == False:
+                test_summary = sh.cell_value(i, 0)
+                test_scenario = str(round(sh.cell_value(i, 1)))
+                effective_date = sh.cell_value(i, 2)
+                contract_class = sh.cell_value(i, 3)
+                agent = sh.cell_value(i, 4)
+                state = sh.cell_value(i, 5)
+                revenue = str(round(sh.cell_value(i, 6)))
+                total_num_records = (sh.cell_value(i, 7))
+                _OLD_scenario = sh.cell_value(i, 8)
+
+            # Else, the cell is empty
+            # End the Loop
+            else:
+                break
 
         ## Select Appropriate URL based on the Environment Value from above
-        baseURL  = Environments.return_environments(environment)
+        base_URL = Environments.return_environments(env)
 
         # Create "Fake" Variables
         #state = frandom.us_state()
@@ -74,76 +120,30 @@ class CreateQuote():
         last_name = name.last_name()
         company_name = company.company_name()
 
-        #company_name_string = "QA Test - The Lance Armstrong Live Strong Company"
-        #address_value = "7021 Cerritos Ave"
-        #city = "Cerritos"
-        #postal_code = "90623"
-
         company_name_string = "QA Test" + " " + "-" + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
         address_value = address.street_address()
         city = StateCapitals.return_state_capital(state)
         postal_code = ZipCodes.return_zip_codes(state)
 
-        revenue = "2500000"
-        total_num_records = '1 to 100,000'
-
         # Access XML to retrieve login credentials
-        tree = ET.parse(os.path.join(config_file_directory, 'resources.xml'))
+        tree = ET.parse(str(config_file_directory /'resources.xml'))
         login_credentials = tree.getroot()
         username = (login_credentials[0][0].text)
         password = (login_credentials[1][1].text)
 
-        # Access XML to retrieve the agent to search for
-        tree = ET.parse(os.path.join(config_file_directory, 'Agents.xml'))
-        agents = tree.getroot()
-        agent = (agents[5][0].text)
-
-        # 0,0 = Crump Tester                -- Wholesale Agent - Crump Insurance Services, Boston - Test Account
-        # 1,0 = Susan Leeming - TEST        -- Sub Agent of Wholesale Agency
-        # 2,0 = Chad Robin                  -- Chad Robin Retail Agent - Robin Insurance
-        # 3,0 = Preferred Agent             -- Preferred Agent - Preferred Agency
-        # 4,0 = TMLT Test User              -- Account to Test COMM2 Scenarios
-        # 5,0 = QA Agent                    -- QA Agent
-        # 6,0 = Janice Quinn                -- 2nd Preferred Agent - ABC Insurance
-
-        # TODO: NEED TO FIX SO THAT SCRIPT USES STRING VALUE CONTAINED IN contract_class variable
-        # Access XML to retrieve contract_class
-
-        # NOTE: For XML, the array count starts at 0
-        # I have inserted a placeholder element at 0 -- Ken
-        # Array will be 1 - 74
-        # For List of Contract Classes, See Contract_Classes.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'Contract_Classes.xml'))
-        contract_classes_XML = tree.getroot()
-        # contract_class = (contract_classes_XML[0][43].text)
-        contract_class = "Property Managers"
-
-        # Retail Sales          - 57
-        # Online Retailer       - 46
-        # Restaurant            - 56
-        # Misc Consultant       - 43
-        # Hospitality           - 30
-        # Title/Escrow Services - 63
-
-        # NOTE: For contract_classes.py, the array count starts at 1
-        # Array will be 1 - 74
-        # contract_class_int_value = ContractClasses.return_contract_class_values(contract_class)
-
-        # To Debug, contract_class, uncomment the next line; set value to an integer from the utilities.contract_classes.py class
-        #contract_class_value = "74"
-
         # Date Variables
         date_today = time.strftime("%m/%d/%Y")
-        ad_hoc_effectiveDate = "07/01/2017"
+        ad_hoc_effectiveDate = "09/06/2017"
 
-        # Initialize Driver; Launch URL
-        # baseURL = "https://service.wn.nasinsurance.com/"
-        driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
+        # Convert effective_date value to format MM/DD/YYYY
+        d = xlrd.xldate_as_tuple(int(effective_date), 0)
+        # convert date tuple in mm-dd-yyyy format
+        d = datetime.datetime(*(d[0:3]))
+        effective_date_formatted = d.strftime("%m/%d/%Y")
 
-        # Maximize Window; Launch URL
-        driver.maximize_window()
-        driver.get(baseURL)
-        driver.implicitly_wait(3)
+        driver.get(base_URL)
+
+        driver.implicitly_wait(60)
 
         # Call Login methods from Pages.home.login_page.py
         lp = LoginPage(driver)
@@ -172,16 +172,11 @@ class CreateQuote():
         ##pp.select_contract_class(contract_class_int_value)
         ##pp.click_continue_on_contract_class_modal_NGP_USPRO()
 
-
         cs = ClientSearch(driver)
         cs.input_bogus_client_data(postal_code)
         cs.manually_input_new_client()
         cs.enter_new_client_name_address(company_name_string, address_value, city, state)
         cc = ClientContact(driver)
-
-        # TODO:
-        # Code now parses URL String & retrieves application ID
-        # cc.parse_url_get_app_id()
 
         # Get the Application ID from URL -- THIS WORKS
         current_url = driver.current_url
@@ -208,79 +203,34 @@ class CreateQuote():
         ### Quote Creation Section  ###
         ###                         ###
 
-        # Create Quote with PCI Option
-        saw_PAF.create_quote_PCI_DSS_No_DQ(total_num_records)
-
-        # Create Quote with NO PCI Option
-        # saw_PAF.create_quote_No_PCI_DSS_No_DQ(total_num_records)
-
-        # Create Quote that Triggers DQ
-
+        #### This section determines if PCI / Non-PCI Coverage Options display
+        if test_scenario == "1":
+            saw_PAF.create_quote_PCI_DSS_No_DQ(total_num_records)
+        elif test_scenario == "2":
+            saw_PAF.create_quote_No_PCI_DSS_No_DQ(total_num_records)
 
         # Click Next on PAF Screen
         saw_PAF.click_next()
 
-        #### This section determines if PCI / Non-PCI Coverage Options display
-        saw_CC_PCI = PCI_Coverage_Options(driver)
-        saw_CC_No_PCI = No_PCI_Coverage_Options(driver)
-
         #### This class is for generic objects that display on the Coverage Options page
         saw_CC = Coverage_Options(driver)
 
+        ### Clear All selections on Coverage Options Screen
         # saw_CC.select_all_deselect_all()
 
-        ### Choose PCI / No PCI Options in this block   ###
-        ###                                             ###
+        ### If / Then Block to determine which instance of Coverage Options to use
 
-        ### PCI Options ###
+        ### PCI & Non-PCI Test Scenarios
 
-        # saw_CC_PCI.select_250K_limit_500_Deductible()
-        # saw_CC_PCI.select_250K_limit_1K_Deductible()
-        # saw_CC_PCI.select_250K_limit_2pt5K_Deductible()
-        # saw_CC_PCI.select_250K_limit_5K_Deductible()
-        # saw_CC_PCI.select_250K_limit_10K_Deductible()
-        # saw_CC_PCI.select_250K_limit_25K_Deductible()
-        # saw_CC_PCI.select_500K_limit_500_Deductible()
-        # saw_CC_PCI.select_500K_limit_1K_Deductible()
-        # saw_CC_PCI.select_500K_limit_2pt5K_Deductible()
-        # saw_CC_PCI.select_500K_limit_5K_Deductible()
-        # saw_CC_PCI.select_500K_limit_10K_Deductible()
-        saw_CC_PCI.select_500K_limit_25K_Deductible()
-        # saw_CC_PCI.select_1MM_limit_500_Deductible()
-        # saw_CC_PCI.select_1MM_limit_1K_Deductible()
-        # saw_CC_PCI.select_1MM_limit_2pt5K_Deductible()
-        # saw_CC_PCI.select_1MM_limit_5K_Deductible()
-        # saw_CC_PCI.select_1MM_limit_10K_Deductible()
-        # saw_CC_PCI.select_1MM_limit_25K_Deductible()
-        # saw_CC_PCI.select_250K_500K_1MM_2MM_limit_500_Deductible()
-        # saw_CC_PCI.select_250K_500K_limit_500_Deductible()
-        # saw_CC_PCI.select_250K_500K_1MM_2MM_limit_2pt5K_Deductible()
+        ### PCI Scenarios
+        if test_scenario == "1":
+            saw_CC_in_use = PCI_Coverage_Options(driver)
+            getattr(saw_CC_in_use, _OLD_scenario)()
 
-        ### No-PCI Options ###
-
-        # saw_CC_No_PCI.select_250K_limit_500_Deductible()
-        # saw_CC_No_PCI.select_250K_limit_1K_Deductible()
-        # saw_CC_No_PCI.select_250K_limit_2pt5K_Deductible()
-        # saw_CC_No_PCI.select_250K_limit_5K_Deductible()
-        # saw_CC_No_PCI.select_250K_limit_10K_Deductible()
-        # saw_CC_No_PCI.select_250K_limit_25K_Deductible()
-        # saw_CC_No_PCI.select_500K_limit_500_Deductible()
-        # saw_CC_No_PCI.select_500K_limit_1K_Deductible()
-        # saw_CC_No_PCI.select_500K_limit_2pt5K_Deductible()
-        # saw_CC_No_PCI.select_500K_limit_5K_Deductible()
-        # saw_CC_No_PCI.select_500K_limit_10K_Deductible()
-        # saw_CC_No_PCI.select_500K_limit_25K_Deductible()
-        # saw_CC_No_PCI.select_1MM_limit_500_Deductible()
-        # saw_CC_No_PCI.select_1MM_limit_1K_Deductible()
-        # saw_CC_No_PCI.select_1MM_limit_2pt5K_Deductible()
-        # saw_CC_No_PCI.select_1MM_limit_5K_Deductible()
-        # saw_CC_No_PCI.select_1MM_limit_10K_Deductible()
-        # saw_CC_No_PCI.select_1MM_limit_25K_Deductible()
-        # saw_CC_No_PCI.select_250K_500K_1MM_2MM_limit_500_Deductible()
-        # saw_CC_No_PCI.select_250K_500K_limit_500_Deductible()
-        # saw_CC_No_PCI.select_250K_500K_1MM_2MM_limit_2pt5K_Deductible()
-
-        #saw_CC.select_all_deselect_all()
+        ### Non-PCI Scenarios
+        elif test_scenario == "2":
+            saw_CC_in_use = No_PCI_Coverage_Options(driver)
+            getattr(saw_CC_in_use, _OLD_scenario)()
 
         saw_CC.click_proceed_to_quote()
 
@@ -306,12 +256,6 @@ class CreateQuote():
         saw_confirm_issue.click_return_to_Admin_Interface()
 
         time.sleep(2)
-
-        #This section is necessary ONLY on STAGE
-        # Call Login methods from Pages.home.login_page.py
-        #lp = LoginPage(driver)
-        #lp.login(username, password)
-        #nb = NavigationBar(driver)
 
         # Click Applications link on Navigation Bar
         nb.click_applications()
@@ -408,13 +352,8 @@ class CreateQuote():
         # Click Policy
         ap.click_policy_link(policy_text)
 
-        # Code works up to this point
-
         # Wait
         driver.implicitly_wait(3)
 
         # Close Browser
         driver.quit()
-
-cq = CreateQuote()
-cq.test_login_search_for_agent_create_quote()
