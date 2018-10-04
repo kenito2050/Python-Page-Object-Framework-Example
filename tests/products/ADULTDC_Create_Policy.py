81e680953cc6c9f54b47c92ab92b249fb31d4c65
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-04 13:51:10 -0700

    Checking in latest updates for NASONLINE Scripts
    ADULTDC_Create_Policy
    
    Scripts runs successfully unattended

--- tests/products/ADULTDC_Create_Policy.py
+++ tests/products/ADULTDC_Create_Policy.py
@@ -43,26 +43,26 @@ from utilities.Environments.Environments import Environments
 from utilities.contract_classes.contract_classes_Medical import ContractClasses_Medical
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes.zip_codes import ZipCodes
+from config_globals import *
 
+class TestCreateQuote():
 
-class CreateQuote():
-
-    def test_login_search_for_agent_create_quote(self):
+    def test_login_search_for_agent_create_quote(self, browser, env):
 
         Product = "ADULTDC"
+        driver = browser
 
         ## Directory Locations
 
-        tests_directory = os.path.abspath(os.pardir)
-        framework_directory = os.path.abspath(os.path.join(tests_directory, os.pardir))
-        config_file_directory = os.path.abspath(os.path.join(framework_directory, 'config_files'))
-        test_case_directory = os.path.abspath(os.path.join(framework_directory, 'utilities\Excel_Sheets\Products'))
-        test_results_directory = os.path.abspath(
-            os.path.join(framework_directory, 'utilities\Excel_Sheets\Test_Results'))
+        tests_directory = ROOT_DIR / 'tests'
+        framework_directory = ROOT_DIR
+        config_file_directory = CONFIG_PATH
+        test_case_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Products'
+        test_results_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Test_Results'
 
         # Determine the Test Run Type
         # Get Test Run Type Text from config file
-        tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
+        tree = ET.parse(str(config_file_directory / 'test_environment.xml'))
         test_environment = tree.getroot()
         test_run_type = (test_environment[1][0].text)
         test_run_type_value = ''
@@ -82,7 +82,7 @@ class CreateQuote():
         global _OLD_scenario_number
 
         # Open Test Scenario Workbook; Instantiate worksheet object
-        wb = xlrd.open_workbook(os.path.join(test_case_directory, Product + '.xlsx'))
+        wb = xlrd.open_workbook(str(test_case_directory / Product) + '.xlsx')
         sh = wb.sheet_by_index(0)
 
         ## Begin For Loop to iterate through Test Scenarios
@@ -123,12 +123,12 @@ class CreateQuote():
         ## Determine Test Environment to run scripts
 
         ## Read in value from test_environment.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
+        tree = ET.parse(str(config_file_directory / 'test_environment.xml'))
         test_environment = tree.getroot()
         environment = (test_environment[0][0].text)
 
         ## Select Appropriate URL based on the Environment Value from above
-        base_URL = Environments.return_environments(environment)
+        base_URL = Environments.return_environments(env)
 
         first_name = name.first_name()
         last_name = name.last_name()
@@ -157,10 +157,10 @@ class CreateQuote():
 
         # Initialize Driver; Launch URL
         # baseURL = "https://svcdemo2.wn.nasinsurance.com/"
-        driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
+        # driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
 
         # Maximize Window; Launch URL
-        driver.maximize_window()
+        # driver.maximize_window()
         driver.get(base_URL)
         driver.implicitly_wait(3)
 
@@ -228,7 +228,7 @@ class CreateQuote():
         #### This class is for generic objects that display on the Coverage Options page
         saw_CC = Coverage_Options(driver)
 
-        # saw_CC.select_all_deselect_all()
+        saw_CC.select_all_deselect_all()
 
         saw_CC.click_proceed_to_quote()
         saw_summary = Summary(driver)
@@ -354,6 +354,3 @@ class CreateQuote():
 
         # Close Browser
         driver.quit()
-
-cq = CreateQuote()
-cq.test_login_search_for_agent_create_quote()
