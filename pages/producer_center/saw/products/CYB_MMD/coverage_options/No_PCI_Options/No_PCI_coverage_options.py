8d3fe9fbd8b03b4b319aa503153b4ccb77edf95c
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-10 14:45:06 -0700

    Checking in latest updates for NASONLINE Scripts
    Added scripts for CYB_MMD - PCI and No PCI
    
    Scripts run at command line but get following message
    
    ConnectionAbortedError: [WinError 10053] An established connection was aborted by the software in your host machine

--- pages/producer_center/saw/products/CYB_MMD/coverage_options/No_PCI_Options/No_PCI_coverage_options.py
+++ pages/producer_center/saw/products/CYB_MMD/coverage_options/No_PCI_Options/No_PCI_coverage_options.py
@@ -7,86 +7,42 @@ class No_PCI_Coverage_Options():
 
     def PageElements(self):
 
-        # e-MD™ Only - No PCI
+        # MEDEFENSE™ Plus Only
 
         # Limits
-
-        # 500K/1MM
-        self.option_850_limit_3169 = self.driver.find_element(By.ID, "option-850-limit-3169")
 
         # 1MM/1MM
-        self.option_850_limit_3168 = self.driver.find_element(By.ID, "option-850-limit-3168")
+        self.option_507_limit_2294 = self.driver.find_element(By.ID, "option-507-limit-2294")
 
         # Deductibles
         # $0
-        self.option_850_deductible_1296 = self.driver.find_element(By.ID, "option-850-deductible-1296")
+        self.option_507_deductible_877 = self.driver.find_element(By.ID, "option-507-deductible-877")
 
-        # $2M e-MD™ Only - No PCI
+
+        # CyberProtector™ Only with Crime - No PCI
 
         # Limits
 
-        # 2MM/2MM
-        self.option_856_limit_3196 = self.driver.find_element(By.ID, "option-856-limit-3196")
+        # 1MM/1MM
+        self.option_1051_limit_4019 = self.driver.find_element(By.ID, "option-1051-limit-4019")
 
         # Deductibles
         # $0
-        self.option_856_deductible_1296 = self.driver.find_element(By.ID, "option-856-deductible-1296")
+        self.option_1051_deductible_1592 = self.driver.find_element(By.ID, "option-1051-deductible-1592")
 
-        # $3M e-MD™ Only - No PCI
-
-        # Limits
-
-        # 3MM/3MM
-        self.option_858_limit_3201 = self.driver.find_element(By.ID, "option-858-limit-3201")
-
-        # Deductibles
-        # $0
-        self.option_858_deductible_1296 = self.driver.find_element(By.ID, "option-858-deductible-1296")
-
-        # $2M Broad Regulatory Protection Plus and e-MD™ Combined - No PCI
-
-        # Limits
-
-        # 2MM/2MM
-        self.option_857_limit_3257 = self.driver.find_element(By.ID, "option-857-limit-3257")
-
-        # Deductibles
-        # $0
-        self.option_857_deductible_1297 = self.driver.find_element(By.ID, "option-857-deductible-1297")
-
-        # $3M Broad Regulatory Protection Plus and e-MD™ Combined - No PCI
+        # MEDEFENSE™ Plus and CyberProtector™ Combined with Crime - No PCI
 
         # Limits
 
         # 3MM/3MM
-        self.option_859_limit_3263 = self.driver.find_element(By.ID, "option-859-limit-3263")
+        self.option_1052_limit_4020 = self.driver.find_element(By.ID, "option-1052-limit-4020")
 
         # Deductibles
         # $0
-        self.option_859_deductible_1297 = self.driver.find_element(By.ID, "option-859-deductible-1297")
+        self.option_1052_deductible_1589 = self.driver.find_element(By.ID, "option-1052-deductible-1589")
 
         return self
 
-    def select_eMD_Only_No_PCI_500K_1MM_limit_0_Deduct(self):
-        No_PCI_Coverage_Options.PageElements(self).option_850_limit_3169.click()
-        No_PCI_Coverage_Options.PageElements(self).option_850_deductible_1296.click()
-
-    def select_eMD_Only_No_PCI_1MM_1MM_limit_0_Deduct(self):
-        No_PCI_Coverage_Options.PageElements(self).option_850_limit_3168.click()
-        No_PCI_Coverage_Options.PageElements(self).option_850_deductible_1296.click()
-
-    def select_2M_eMD_Only_No_PCI_2MM_2MM_limit_0_Deduct(self):
-        No_PCI_Coverage_Options.PageElements(self).option_856_limit_3196.click()
-        No_PCI_Coverage_Options.PageElements(self).option_856_deductible_1296.click()
-
-    def select_3M_eMD_Only_No_PCI_3MM_3MM_limit_0_Deduct(self):
-        No_PCI_Coverage_Options.PageElements(self).option_858_limit_3201.click()
-        No_PCI_Coverage_Options.PageElements(self).option_858_deductible_1296.click()
-
-    def select_2M_Broad_Regulatory_Protection_Plus_and_eMD_Combined_No_PCI_2MM_2MM_limit_0_Deduct(self):
-        No_PCI_Coverage_Options.PageElements(self).option_857_limit_3257.click()
-        No_PCI_Coverage_Options.PageElements(self).option_857_deductible_1297.click()
-
-    def select_3M_Broad_Regulatory_Protection_Plus_and_eMD_Combined_No_PCI_3MM_3MM_limit_0_Deduct(self):
-        No_PCI_Coverage_Options.PageElements(self).option_859_limit_3263.click()
-        No_PCI_Coverage_Options.PageElements(self).option_859_deductible_1297.click()
+    def select_CyberProtector_Only_with_Crime_No_PCI(self):
+        No_PCI_Coverage_Options.PageElements(self).option_507_limit_2294.click()
+        No_PCI_Coverage_Options.PageElements(self).option_507_deductible_877.click()
--- pages/producer_center/saw/products/CYB_MMD/coverage_options/PCI_Options/PCI_coverage_options.py
+++ pages/producer_center/saw/products/CYB_MMD/coverage_options/PCI_Options/PCI_coverage_options.py
@@ -7,89 +7,43 @@ class PCI_Coverage_Options():
 
     def PageElements(self):
 
-        # e-MD™ Only
+        # MEDEFENSE™ Plus Only
 
         # Limits
-
-        # 500K/1MM
-        self.option_451_limit_1989 = self.driver.find_element(By.ID, "option-451-limit-1989")
 
         # 1MM/1MM
-        self.option_451_limit_1988 = self.driver.find_element(By.ID, "option-451-limit-1988")
+        self.option_507_limit_2294 = self.driver.find_element(By.ID, "option-507-limit-2294")
 
         # Deductibles
         # $0
-        self.option_451_deductible_763 = self.driver.find_element(By.ID, "option-451-deductible-763")
+        self.option_507_deductible_877 = self.driver.find_element(By.ID, "option-507-deductible-877")
 
 
-        # $2M e-MD™ Only
+        # CyberProtector™ Only w/ Crime
 
         # Limits
 
-        # 2MM/2MM
-        self.option_852_limit_3186 = self.driver.find_element(By.ID, "option-852-limit-3186")
+        # 1MM/1MM
+        self.option_1053_limit_4021 = self.driver.find_element(By.ID, "option-1053-limit-4021")
 
         # Deductibles
         # $0
-        self.option_852_deductible_763 = self.driver.find_element(By.ID, "option-852-deductible-763")
+        self.option_1053_deductible_1590 = self.driver.find_element(By.ID, "option-1053-deductible-1590")
 
-        # $3M e-MD™ Only
-
-        # Limits
-
-        # 3MM/3MM
-        self.option_853_limit_3187 = self.driver.find_element(By.ID, "option-853-limit-3187")
-
-        # Deductibles
-        # $0
-        self.option_853_deductible_763 = self.driver.find_element(By.ID, "option-853-deductible-763")
-
-
-        # $2M Broad Regulatory Protection Plus and e-MD™ Combined
-
-        # Limits
-
-        # 2MM/2MM
-        self.option_854_limit_3256 = self.driver.find_element(By.ID, "option-854-limit-3256")
-
-        # Deductibles
-        # $0
-        self.option_854_deductible_1297 = self.driver.find_element(By.ID, "option-854-deductible-1297")
-
-        # $3M Broad Regulatory Protection Plus and e-MD™ Combined
+        # MEDEFENSE™ Plus and CyberProtector™ Combined w/ Crime
 
         # Limits
 
         # 3MM/3MM
-        self.option_855_limit_3262 = self.driver.find_element(By.ID, "option-855-limit-3262")
+        self.option_1054_limit_4022 = self.driver.find_element(By.ID, "option-1054-limit-4022")
 
         # Deductibles
         # $0
-        self.option_855_deductible_762 = self.driver.find_element(By.ID, "option-855-deductible-762")
+        self.option_1054_deductible_1591 = self.driver.find_element(By.ID, "option-1054-deductible-1591")
 
 
         return self
 
-    def select_eMD_Only_500K_1MM_limit_0_Deduct(self):
-        PCI_Coverage_Options.PageElements(self).option_451_limit_1989.click()
-        PCI_Coverage_Options.PageElements(self).option_451_deductible_763.click()
-
-    def select_eMD_Only_1MM_1MM_limit_0_Deduct(self):
-        PCI_Coverage_Options.PageElements(self).option_451_limit_1988.click()
-        PCI_Coverage_Options.PageElements(self).option_451_deductible_763.click()
-
-    def select_2M_eMD_Only_2MM_2MM_limit_0_Deduct(self):
-        PCI_Coverage_Options.PageElements(self).option_852_limit_3186.click()
-        PCI_Coverage_Options.PageElements(self).option_852_deductible_763.click()
-
-    def select_3M_eMD_Only_3MM_3MM_limit_0_Deduct(self):
-        PCI_Coverage_Options.PageElements(self).option_853_limit_3187.click()
-        PCI_Coverage_Options.PageElements(self).option_853_deductible_763.click()
-
-    def select_2M_Broad_Regulatory_Protection_Plus_and_eMD_Combined_2MM_2MM_limit_0_Deduct(self):
-        PCI_Coverage_Options.PageElements(self).option_854_limit_3256.click()
-        PCI_Coverage_Options.PageElements(self).option_854_deductible_1297.click()
-
-    def select_3M_Broad_Regulatory_Protection_Plus_and_eMD_Combined_3MM_3MM_limit_0_Deduct(self):
-        PCI_Coverage_Options.PageElements(self).option_855_limit_3262.click()
-        PCI_Coverage_Options.PageElements(self).option_855_deductible_762.click()
+    def select_CyberProtector_Only(self):
+        PCI_Coverage_Options.PageElements(self).option_507_limit_2294.click()
+        PCI_Coverage_Options.PageElements(self).option_507_deductible_877.click()
