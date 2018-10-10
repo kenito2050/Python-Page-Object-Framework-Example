40bec1474edf36c8a5e146dd781b844b7cbd433d
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-10 15:52:12 -0700

    Checking in latest updates for NASONLINE Scripts
    CYB_MMIC Ballpark.

--- tests/products/CYB_MMIC_Create_Ballpark.py
+++ tests/products/CYB_MMIC_Create_Ballpark.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 import os
 from xml.etree import ElementTree as ET
@@ -6,6 +7,7 @@ from faker import address
 from faker import company
 from faker import name
 from selenium import webdriver
+import xlrd
 
 from pages.producer_center.ballpark.ballpark_Indication import BallPark_Indication
 from pages.producer_center.ballpark.ballpark_PAF import BallPark_PAF
@@ -19,95 +21,111 @@ from utilities.Environments.Environments import Environments
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes_state_capitals.zip_codes import ZipCodes
 import time
+from config_globals import *
 
+class TestCreateQuote():
 
-class CreateQuote():
+    def test_login_search_for_agent_create_quote(self, browser, env):
 
-    def test_login_search_for_agent_create_quote(self):
+        Product = "CYB_MMIC"
+        driver = browser
 
         ## Directory Locations
 
-        tests_directory = os.path.abspath(os.pardir)
-        framework_directory = os.path.abspath(os.path.join(tests_directory, os.pardir))
-        config_file_directory = os.path.abspath(os.path.join(framework_directory, 'config_files'))
-        test_case_directory = os.path.abspath(os.path.join(framework_directory, 'utilities\Excel_Sheets\Products'))
-        test_results_directory = os.path.abspath(os.path.join(framework_directory, 'utilities\Excel_Sheets\Test_Results'))
+        tests_directory = ROOT_DIR / 'tests'
+        framework_directory = ROOT_DIR
+        config_file_directory = CONFIG_PATH
+        test_case_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Products'
+        test_results_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Test_Results'
+
+        global test_summary
+        global test_scenario
+        global effective_date
+        global contract_class
+        global agent
+        global state
+        global revenue
+        global staff_count
+        global _OLD_scenario
+        global _OLD_scenario_number
+
+        # Open Test Scenario Workbook; Instantiate worksheet object
+        # 0 - First Worksheet
+        # 1 - Second Worksheet...etc
+
+        wb = xlrd.open_workbook(str(test_case_directory / Product) + '.xlsx')
+        sh = wb.sheet_by_index(3)
+
+        ## Begin For Loop to iterate through Test Scenarios
+        i = 1
+        rows = sh.nrows
+        empty_cell = False
+        for x in range(1, sh.nrows):
+
+            cell_val = sh.cell(i, 0).value
+            if cell_val == '':
+                # If Cell Value is empty, set empty_cell to True
+                empty_cell = True
+            else:
+                # If Cell Value is NOT empty, set empty_cell to False
+                empty_cell = False
+
+            # Check to see if cell is NOT empty
+            # If cell is not empty, read in the values
+            if empty_cell == False:
+                test_summary = sh.cell_value(i, 0)
+                test_scenario = str(round(sh.cell_value(i, 1)))
+                effective_date = sh.cell_value(i, 2)
+                contract_class = sh.cell_value(i, 3)
+                agent = sh.cell_value(i, 4)
+                state = sh.cell_value(i, 5)
+                revenue = str(round(sh.cell_value(i, 6)))
+                staff_count = str(round(sh.cell_value(i, 7)))
+                _OLD_scenario = sh.cell_value(i, 8)
+                _OLD_scenario_number = str(round(sh.cell_value(i, 9)))
+
+            # Else, the cell is empty
+            # End the Loop
+            else:
+                break
 
         ## Determine Test Environment to run scripts
 
-        ## Read in value from test_environment.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
-        test_environment  = tree.getroot()
-        environment =(test_environment[0][0].text)
-
         ## Select Appropriate URL based on the Environment Value from above
-        baseURL  = Environments.return_environments(environment)
+        baseURL  = Environments.return_environments(env)
 
         # Create "Fake" Variables
         #state = frandom.us_state()
-        state = "North Carolina"
+        # state = "North Carolina"
         #state = Create_Insured_Address.return_alabama(state_value)
         first_name = name.first_name()
         last_name = name.last_name()
         company_name = company.company_name()
         #company_name_string = company_name
         company_name_string = "QA Ballpark Test" + " " + "-" + " " + "Dr." + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
-        address_value = address.street_address()
-        city = StateCapitals.return_state_capital(state)
         postal_code = ZipCodes.return_zip_codes(state)
 
-        revenue = "9000000"
-        staff_count = "101"
-
         # Access XML to retrieve login credentials
-        tree = ET.parse(os.path.join(config_file_directory, 'resources.xml'))
+        tree = ET.parse(str(config_file_directory /  'resources.xml'))
         login_credentials = tree.getroot()
         username = (login_credentials[0][0].text)
         password = (login_credentials[1][1].text)
 
-        # Access XML to retrieve the agent to search for
-        tree = ET.parse(os.path.join(config_file_directory, 'Agents.xml'))
-        agents = tree.getroot()
-        agent = (agents[5][0].text)
-
-        # 0,0 = Crump Tester                -- Wholesale Agent - Crump Insurance Services, Boston - Test Account
-        # 1,0 = Susan Leeming - TEST        -- Sub Agent of Wholesale Agency
-        # 2,0 = Retail Agent                -- Retail Agent - Boston Retail Insurance
-        # 3,0 = Preferred Agent             -- Preferred Agent - Preferred Agency
-        # 4,0 = TMLT Test User              -- Account to Test COMM2 Scenarios
-        # 5,0 = QA Agent                    -- QA Agent
-        # 6,0 = 2nd Preferred Agent         -- 2nd Preferred Agent - ABC Insurance
-
-        # TODO: NEED TO FIX SO THAT SCRIPT USES STRING VALUE CONTAINED IN contract_class variable
-        # Access XML to retrieve contract_class
-
-        # NOTE: For XML, the array count starts at 0
-        # I have inserted a placeholder element at 0 -- Ken
-        # Array will be 1 - 74
-        # For List of Contract Classes, See Contract_Classes.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'Contract_Classes_Medical.xml'))
-        contract_classes_XML = tree.getroot()
-        contract_class = (contract_classes_XML[0][1].text)
-        # Contract Class - 1 - Medical Group
-        # Contract Class - 2 - Office of Physician
-        # Contract Class - 3 - Office of Dentists
-
-        # NOTE: For contract_classes.py, the array count starts at 1
-        # Array will be 1 - 74
-        contract_class_int_value = ContractClasses_Medical.return_contract_class_values(contract_class)
-
-        # To Debug, contract_class, uncomment the next line; set value to an integer from the utilities.contract_classes.py class
-        #contract_class_value = "74"
-
         date_today = time.strftime("%m/%d/%Y")
-        ad_hoc_effectiveDate = "07/01/2017"
+
+        # Convert effective_date value to format MM/DD/YYYY
+        d = xlrd.xldate_as_tuple(int(effective_date), 0)
+        # convert date tuple in mm-dd-yyyy format
+        d = datetime.datetime(*(d[0:3]))
+        effective_date_formatted = d.strftime("%m/%d/%Y")
+
 
         # Initialize Driver; Launch URL
         # baseURL = "https://svcrel.wn.nasinsurance.com/"
-        driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
+        # driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
 
         # Maximize Window; Launch URL
-        driver.maximize_window()
+        # driver.maximize_window()
         driver.get(baseURL)
         driver.implicitly_wait(3)
 
@@ -133,10 +151,10 @@ class CreateQuote():
         time.sleep(3)
 
         # Enter Ad Hoc Effective Date
-        # bp_PAF.enter_effective_date(ad_hoc_effectiveDate)
+        bp_PAF.enter_effective_date(effective_date_formatted)
 
         # Enter Today's Date as Effective Date
-        bp_PAF.enter_current_date(date_today)
+        # bp_PAF.enter_current_date(date_today)
 
         time.sleep(3)
         bp_PAF.click_doctor_count_field()
@@ -164,6 +182,3 @@ class CreateQuote():
 
         # Close Browser
         driver.quit()
-
-cq = CreateQuote()
-cq.test_login_search_for_agent_create_quote()
