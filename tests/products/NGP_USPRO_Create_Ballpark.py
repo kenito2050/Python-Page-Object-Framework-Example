1273279c01e83f9a78100d3623b0d944f019603b
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-11 11:29:51 -0700

    Checking in latest updates for NASONLINE Scripts
    USPRO Ballpark
    
    Script runs successfully at command line

--- tests/products/NGP_USPRO_Create_Ballpark.py
+++ tests/products/NGP_USPRO_Create_Ballpark.py
@@ -1,12 +1,13 @@
-import unittest
+import datetime
 import os
+import time
+from urllib.parse import urlparse, parse_qs
 from xml.etree import ElementTree as ET
 
+import xlrd
 from faker import address
 from faker import company
 from faker import name
-from selenium import webdriver
-import time
 
 from pages.producer_center.ballpark.ballpark_Indication import BallPark_Indication
 from pages.producer_center.ballpark.ballpark_PAF import BallPark_PAF
@@ -15,157 +16,176 @@ from pages.producer_center.products_programs_page import ProductsAndPrograms
 from pages.service_center.agents_page import AgentsPage
 from pages.service_center.login_page import LoginPage
 from pages.service_center.navigation_bar import NavigationBar
+from utilities.contract_classes.contract_classes_Medical import ContractClasses_Medical
 from utilities.Environments.Environments import Environments
-from utilities.contract_classes.contract_classes import ContractClasses
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes_state_capitals.zip_codes import ZipCodes
+import time
+from config_globals import *
 
 
-class CreateQuote():
+class TestCreateQuote:
 
-    def test_login_search_for_agent_create_quote(self):
+    def test_login_search_for_agent_create_quote(self, browser, env):
+
+        Product = "NGP_USPRO_BALLPARK"
+        driver = browser
 
         ## Directory Locations
 
-        tests_directory = os.path.abspath(os.pardir)
-        framework_directory = os.path.abspath(os.path.join(tests_directory, os.pardir))
-        config_file_directory = os.path.abspath(os.path.join(framework_directory, 'config_files'))
-        test_case_directory = os.path.abspath(os.path.join(framework_directory, 'utilities\Excel_Sheets\Products'))
-        test_results_directory = os.path.abspath(
-            os.path.join(framework_directory, 'utilities\Excel_Sheets\Test_Results'))
+        tests_directory = ROOT_DIR / 'tests'
+        framework_directory = ROOT_DIR
+        config_file_directory = CONFIG_PATH
+        test_case_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Products'
+        test_results_directory = framework_directory / 'utilities' / 'Excel_Sheets' / 'Test_Results'
 
-        ## Determine Test Environment to run scripts
+        global test_summary
+        global test_scenario
+        global effective_date
+        global test_scenario_number
+        global regression
+        global smoke
+        global sanity
+        global contract_class
+        global agent
+        global state
+        global revenue
+        global total_num_records
+        global limit
+        global deductible
+        global _OLD_scenario
+        global _OLD_scenario_number
 
-        ## Read in value from test_environment.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
-        test_environment  = tree.getroot()
-        environment =(test_environment[0][0].text)
+        # Open Test Scenario Workbook; Instantiate worksheet object
+        # 0 - First Worksheet
+        # 1 - Second Worksheet...etc
 
-        ## Select Appropriate URL based on the Environment Value from above
-        baseURL  = Environments.return_environments(environment)
+        wb = xlrd.open_workbook(str(test_case_directory / Product) + '.xlsx')
+        sh = wb.sheet_by_index(1)
 
-        # Create "Fake" Variables
-        #state = frandom.us_state()
-        state = "California"
-        #state = Create_Insured_Address.return_alabama(state_value)
-        first_name = name.first_name()
-        last_name = name.last_name()
-        company_name = company.company_name()
-        #company_name_string = company_name
-        company_name_string = "QA Ballpark Test" + " " + "-" + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
-        address_value = address.street_address()
-        city = StateCapitals.return_state_capital(state)
-        postal_code = ZipCodes.return_zip_codes(state)
+        ## Begin For Loop to iterate through Test Scenarios
+        i = 1
+        rows = sh.nrows
+        empty_cell = False
+        for i in range(1, sh.nrows):
 
-        revenue = "1000000"
+            cell_val = sh.cell(i, 0).value
+            if cell_val == '':
+                # If Cell Value is empty, set empty_cell to True
+                empty_cell = True
+            else:
+                # If Cell Value is NOT empty, set empty_cell to False
+                empty_cell = False
 
-        # Access XML to retrieve login credentials
-        tree = ET.parse(os.path.join(config_file_directory, 'resources.xml'))
-        login_credentials = tree.getroot()
-        username = (login_credentials[0][0].text)
-        password = (login_credentials[0][1].text)
+            # Check to see if cell is NOT empty
+            # If cell is not empty, read in the values
+            if empty_cell == False:
+                test_summary = sh.cell_value(i, 0)
+                test_scenario = str(round(sh.cell_value(i, 1)))
+                effective_date = sh.cell_value(i, 2)
+                test_scenario_number = str(round(sh.cell_value(i, 3)))
+                regression = sh.cell_value(i, 4)
+                smoke = sh.cell_value(i, 5)
+                sanity = sh.cell_value(i, 6)
+                contract_class = sh.cell_value(i, 7)
+                agent = sh.cell_value(i, 8)
+                state = sh.cell_value(i, 9)
+                revenue = str(round(sh.cell_value(i, 10)))
+                total_num_records = (sh.cell_value(i, 11))
+                _OLD_scenario = sh.cell_value(i, 12)
+                _OLD_scenario_number = str(round(sh.cell_value(i, 13)))
 
-        # Access XML to retrieve the agent to search for
-        tree = ET.parse(os.path.join(config_file_directory, 'Agents.xml'))
-        agents = tree.getroot()
-        agent = (agents[5][0].text)
+            # Else, the cell is empty
+            # End the Loop
+            else:
+                break
 
-        # 0,0 = Crump Tester                -- Wholesale Agent - Crump Insurance Services, Boston - Test Account
-        # 1,0 = Susan Leeming - TEST        -- Sub Agent of Wholesale Agency
-        # 2,0 = Retail Agent                -- Retail Agent - Boston Retail Insurance
-        # 3,0 = Preferred Agent             -- Preferred Agent - Preferred Agency
-        # 4,0 = TMLT Test User              -- Account to Test COMM2 Scenarios
-        # 5,0 = QA Agent                    -- QA Agent
-        # 6,0 = 2nd Preferred Agent         -- 2nd Preferred Agent - ABC Insurance
+            ## Determine Test Environment to run scripts
 
-        # TODO: NEED TO FIX SO THAT SCRIPT USES STRING VALUE CONTAINED IN contract_class variable
-        # Access XML to retrieve contract_class
+            ## Read in value from test_environment.xml
+            # tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
+            # test_environment = tree.getroot()
+            # environment = (test_environment[0][0].text)
 
-        # NOTE: For XML, the array count starts at 0
-        # I have inserted a placeholder element at 0 -- Ken
-        # Array will be 1 - 74
-        # For List of Contract Classes, See Contract_Classes.xml
-        tree = ET.parse(os.path.join(config_file_directory, 'Contract_Classes.xml'))
-        contract_classes_XML = tree.getroot()
-        contract_class = (contract_classes_XML[0][63].text)
+            ## Select Appropriate URL based on the Environment Value from above
+            base_URL = Environments.return_environments(env)
 
-        # NOTE: For contract_classes.py, the array count starts at 1
-        # Array will be 1 - 74
-        contract_class_int_value = ContractClasses.return_contract_class_values(contract_class)
+            first_name = name.first_name()
+            last_name = name.last_name()
+            company_name = company.company_name()
+            # company_name_string = company_name
+            company_name_string = "QA Test" + " " + "-" + " " + "Dr." + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
+            address_value = address.street_address()
+            city = StateCapitals.return_state_capital(state)
+            postal_code = ZipCodes.return_zip_codes(state)
 
-        # To Debug, contract_class, uncomment the next line; set value to an integer from the utilities.contract_classes.py class
-        # 'Accounting, Auditing, and Bookkeeping': '1',
-        #'Business Consulting': '7',
-        #'Online Retailer': '46'
-        #'Retail Sales': '57'
-        #'Title/Escrow Services': '63'
+            # Access XML to retrieve login credentials
+            tree = ET.parse(str(config_file_directory /'resources.xml'))
+            login_credentials = tree.getroot()
+            username = (login_credentials[1][0].text)
+            password = (login_credentials[1][1].text)
 
-        # Date Variables
-        date_today = time.strftime("%m/%d/%Y")
-        ad_hoc_effectiveDate = "07/01/2017"
+            # Date Variables
+            date_today = time.strftime("%m/%d/%Y")
+            ad_hoc_effectiveDate = "09/06/2017"
 
-        # Initialize Driver; Launch URL
-        # baseURL = "https://svcdemo4.wn.nasinsurance.com/"
-        driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
+            # Convert effective_date value to format MM/DD/YYYY
+            d = xlrd.xldate_as_tuple(int(effective_date), 0)
+            # convert date tuple in mm-dd-yyyy format
+            d = datetime.datetime(*(d[0:3]))
+            effective_date_formatted = d.strftime("%m/%d/%Y")
 
-        # Maximize Window; Launch URL
-        driver.maximize_window()
-        driver.get(baseURL)
-        driver.implicitly_wait(3)
+            # driver.get(baseURL)
 
-        # Call Login methods from Pages.home.login_page.py
-        lp = LoginPage(driver)
-        lp.login(username, password)
-        lp.click_login_button()
-        nb = NavigationBar(driver)
-        nb.click_agents()
-        ap = AgentsPage(driver)
-        ap.search_for_agent(agent)
-        ap.click_submit_new_application_as_agent()
+            driver.get(base_URL)
+            driver.implicitly_wait(3)
 
-        pp = ProductsAndPrograms(driver)
-        pp.click_ballpark()
+            # Call Login methods from Pages.home.login_page.py
+            lp = LoginPage(driver)
+            lp.login(username, password)
+            lp.click_login_button()
+            nb = NavigationBar(driver)
+            nb.click_agents()
+            ap = AgentsPage(driver)
+            ap.search_for_agent(agent)
+            ap.click_submit_new_application_as_agent()
+            pp = ProductsAndPrograms(driver)
+            pp.click_ballpark()
 
-        bp_PAF = BallPark_PAF(driver)
-        bp_PAF.switch_windows()
-        bp_PAF.start_ballpark_enter_faker_company_name_valid_zip(company_name_string, postal_code)
-        bp_PAF.select_contract_class(contract_class)
-        bp_PAF.click_ballpark_button()
+            bp_PAF = BallPark_PAF(driver)
+            bp_PAF.switch_windows()
+            bp_PAF.start_ballpark_enter_faker_company_name_valid_zip(company_name_string, postal_code)
+            bp_PAF.select_contract_class(contract_class)
+            bp_PAF.click_ballpark_button_new()
 
-        bp_PAF.select_NGP_USPRO()
-        time.sleep(3)
+            bp_PAF.select_NGP_USPRO()
+            time.sleep(3)
 
-        # Enter Ad Hoc Effective Date
-        bp_PAF.enter_effective_date(date_today)
+            # Enter Ad Hoc Effective Date
+            # bp_PAF.enter_effective_date(ad_hoc_effectiveDate)
 
-        # Enter Today's Date as Effective Date
-        # bp_PAF.enter_current_date(date_today)
+            # Enter Today's Date as Effective Date
+            bp_PAF.enter_current_date(date_today)
 
-        time.sleep(3)
-        bp_PAF.enter_revenue(revenue)
-        bp_PAF.click_ballpark_button()
+            time.sleep(3)
+            bp_PAF.enter_revenue(revenue)
+            bp_PAF.click_ballpark_button()
 
-        bp_Indication = BallPark_Indication(driver)
-        bp_Indication.click_Download_Send_Indication()
+            bp_Indication = BallPark_Indication(driver)
+            bp_Indication.click_Download_Send_Indication()
 
-        bp_Download_Send = BallPark_Download_Send(driver)
-        bp_Download_Send.input_email()
-        bp_Download_Send.click_send_email()
+            bp_Download_Send = BallPark_Download_Send(driver)
+            bp_Download_Send.input_email()
+            bp_Download_Send.click_send_email()
 
-        # Close Ballpark Window
-        driver.close()
+            # Close Ballpark Window
+            driver.close()
 
-        # Switch to First Window (Service Center)
-        driver.switch_to.window(driver.window_handles[0])
+            # Switch to First Window (Service Center)
+            driver.switch_to.window(driver.window_handles[0])
 
-        # Close First Window (Service Center)
-        # driver.close()
+            # Close First Window (Service Center)
+            # driver.close()
 
-        # Wait
-        driver.implicitly_wait(3)
-
-        # Close Browser
-        driver.quit()
-
-cq = CreateQuote()
-cq.test_login_search_for_agent_create_quote()
+            # Wait
+            driver.implicitly_wait(3)
