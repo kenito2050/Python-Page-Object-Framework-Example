e5e8c23c539990a372da4cf12d1c28f8fa90eb31
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-22 14:24:07 -0700

    Updated Script Script now uses Data Generator and Date Time Utility Classes

--- tests/products/SMOKE_CYB_OMSNIC_No_PCI_Create_Policy.py
+++ tests/products/SMOKE_CYB_OMSNIC_No_PCI_Create_Policy.py
@@ -1,17 +1,7 @@
-import csv
-import datetime
-import os
 import time
-import unittest
 from urllib.parse import urlparse, parse_qs
 from xml.etree import ElementTree as ET
-
 import xlrd
-from faker import address
-from faker import company
-from faker import name
-from selenium import webdriver
-
 from pages.producer_center.client_contact_page import ClientContact
 from pages.producer_center.client_search_page import ClientSearch
 from pages.producer_center.my_policies.my_policies_screens.active_policies import active_policies
@@ -58,6 +48,8 @@ from pages.service_center.subjectivities import Subjectivities
 from utilities.Environments.Environments import Environments
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes_state_capitals.zip_codes import ZipCodes
+from utilities.Faker.Data_Generator import Data_Generator
+from utilities.Date_Time_Generator.Date_Time_Generator import Date_Time_Generator
 from config_globals import *
 
 class TestCreateQuote:
@@ -130,44 +122,32 @@ class TestCreateQuote:
             else:
                 break
 
-            ## Determine Test Environment to run scripts
+            # Create Instance of Data Generator
+            dg = Data_Generator()
 
-            ## Read in value from test_environment.xml
-            # tree = ET.parse(os.path.join(config_file_directory, 'test_environment.xml'))
-            # test_environment = tree.getroot()
-            # environment = (test_environment[0][0].text)
-
-            ## Select Appropriate URL based on the Environment Value from above
-            base_URL = Environments.return_environments(env)
-
-            first_name = name.first_name()
-            last_name = name.last_name()
-            company_name = company.company_name()
-            # company_name_string = company_name
-            company_name_string = "QA Test" + " " + "-" + " " + "Dr." + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
-            address_value = address.street_address()
+            # Create Company Name Value
+            company_name_string = dg.create_full_company_name()
+            # Create Street Address Value
+            address_value = dg.create_street_address()
             city = StateCapitals.return_state_capital(state)
             postal_code = ZipCodes.return_zip_codes(state)
 
+            # Create Instance of Date Time Generator
+            dtg = Date_Time_Generator()
+            # Create Today's Date
+            date_today = dtg.return_date_today()
+
             # Access XML to retrieve login credentials
-            tree = ET.parse(str(config_file_directory /'resources.xml'))
+            tree = ET.parse(str(config_file_directory / 'resources.xml'))
             login_credentials = tree.getroot()
             username = (login_credentials[1][0].text)
             password = (login_credentials[1][1].text)
 
-            # Date Variables
-            date_today = time.strftime("%m/%d/%Y")
-            ad_hoc_effectiveDate = "09/06/2017"
+            ## Test Environment
+            ## Select Appropriate URL based on the Environment Value (env)
+            baseURL = Environments.return_environments(env)
 
-            # Convert effective_date value to format MM/DD/YYYY
-            d = xlrd.xldate_as_tuple(int(effective_date), 0)
-            # convert date tuple in mm-dd-yyyy format
-            d = datetime.datetime(*(d[0:3]))
-            effective_date_formatted = d.strftime("%m/%d/%Y")
-
-            # driver.get(baseURL)
-
-            driver.get(base_URL)
+            driver.get(baseURL)
 
             driver.implicitly_wait(3)
 
@@ -183,10 +163,6 @@ class TestCreateQuote:
 
             pp = ProductsAndPrograms(driver)
             pp.click_CYB_OMSNIC()
-
-            # The following lines added on 10-09-17 work
-            # pp.click_contract_class_drop_down_select_contract_class(contract_class)
-            # pp.click_continue_on_contract_class_modal()
 
             cs = ClientSearch(driver)
             cs.input_bogus_client_data(postal_code)
@@ -304,17 +280,9 @@ class TestCreateQuote:
             time.sleep(3)
 
             # At this point, script is re-directed to service center login screen
-            # This works on DEV
-            # TODO: FIX redirection; should redirect back to Service Center
             saw_confirm_issue.click_return_to_Admin_Interface()
 
             time.sleep(2)
-
-            # This section is necessary ONLY on STAGE
-            # Call Login methods from Pages.home.login_page.py
-            # lp = LoginPage(driver)
-            # lp.login(username, password)
-            # nb = NavigationBar(driver)
 
             # Click Applications link on Navigation Bar
             nb.click_applications()
@@ -323,10 +291,6 @@ class TestCreateQuote:
             app_page = ApplicationsPage(driver)
             app_page.enter_application_id(application_id)
             app_page.click_search_button()
-
-            # Click on application id link
-            # THIS IS NOT WORKING
-            # app_page.click_application_id_link(application_id)
 
             # Navigate to Application Details page
             new_current_url = driver.current_url
@@ -346,8 +310,6 @@ class TestCreateQuote:
             # Approve Subjectivities
             sub = Subjectivities(driver)
             sub.set_all_subjectivities_to_recieved()
-            # sub.change_open_subjectivities_to_received()
-            # sub.select_yes_to_subjectivities_met()
             sub.click_submit()
             sub.click_agent_link()
 
