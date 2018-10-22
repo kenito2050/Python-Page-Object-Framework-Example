ecc000c77266fbd8a10f071d7623bb427a184fdb
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-22 11:59:37 -0700

    Updated Script Script now uses Data Generator and Date Time Utility Classes

--- tests/products/CYB_SVMIC_Create_Policy_PCI.py
+++ tests/products/CYB_SVMIC_Create_Policy_PCI.py
@@ -1,16 +1,7 @@
-import datetime
-import os
-import time
-import unittest
 from urllib.parse import urlparse, parse_qs
 from xml.etree import ElementTree as ET
-
+import time
 import xlrd
-from faker import address
-from faker import company
-from faker import name
-from selenium import webdriver
-
 from pages.producer_center.client_contact_page import ClientContact
 from pages.producer_center.client_search_page import ClientSearch
 from pages.producer_center.my_policies.my_policies_screens.active_policies import active_policies
@@ -41,9 +32,10 @@ from pages.service_center.policy_screens.effective_periods import Effective_Peri
 from pages.service_center.policy_screens.policy_screens import Policy_Screens
 from pages.service_center.subjectivities import Subjectivities
 from utilities.Environments.Environments import Environments
-from utilities.contract_classes.contract_classes_Medical import ContractClasses_Medical
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes_state_capitals.zip_codes import ZipCodes
+from utilities.Faker.Data_Generator import Data_Generator
+from utilities.Date_Time_Generator.Date_Time_Generator import Date_Time_Generator
 from config_globals import *
 
 class TestCreateQuote():
@@ -75,7 +67,6 @@ class TestCreateQuote():
         global _OLD_scenario
         global _OLD_scenario_number
 
-
         # Open Test Scenario Workbook; Instantiate worksheet object
         wb = xlrd.open_workbook(str(test_case_directory / Product) + '.xlsx')
         sh = wb.sheet_by_index(0)
@@ -93,7 +84,6 @@ class TestCreateQuote():
             else:
                 # If Cell Value is NOT empty, set empty_cell to False
                 empty_cell = False
-
 
             # Check to see if cell is NOT empty
             # If cell is not empty, read in the values
@@ -114,49 +104,34 @@ class TestCreateQuote():
             else:
                 break
 
-            ## Determine Test Environment to run scripts
+            # Create Instance of Data Generator
+            dg = Data_Generator()
 
-            ## Select Appropriate URL based on the Environment Value from above
-            base_URL = Environments.return_environments(env)
-
-            first_name = name.first_name()
-            last_name = name.last_name()
-            company_name = company.company_name()
-            # company_name_string = company_name
-            company_name_string = "QA Test" + " " + "-" + " " + "Dr." + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
-            address_value = address.street_address()
+            # Create Company Name Value
+            company_name_string = dg.create_full_company_name()
+            # Create Street Address Value
+            address_value = dg.create_street_address()
             city = StateCapitals.return_state_capital(state)
             postal_code = ZipCodes.return_zip_codes(state)
 
-            # revenue = "20,000,000"
-            total_num_records = '1 to 100,000'
-            # cpa_count = "9"
+            # Create Instance of Date Time Generator
+            dtg = Date_Time_Generator()
+            # Create Today's Date
+            date_today = dtg.return_date_today()
 
             # Access XML to retrieve login credentials
             tree = ET.parse(str(config_file_directory / 'resources.xml'))
             login_credentials = tree.getroot()
-            username = (login_credentials[0][0].text)
+            username = (login_credentials[1][0].text)
             password = (login_credentials[1][1].text)
 
-            # Date Variables
-            date_today = time.strftime("%m/%d/%Y")
-            ad_hoc_effectiveDate = "09/06/2017"
-
-            # Convert effective_date value to format MM/DD/YYYY
-            d = xlrd.xldate_as_tuple(int(effective_date), 0)
-            # convert date tuple in mm-dd-yyyy format
-            d = datetime.datetime(*(d[0:3]))
-            effective_date_formatted = d.strftime("%m/%d/%Y")
-
-            # Initialize Driver; Launch URL
-            # baseURL = "https://svcdemo1.wn.nasinsurance.com/"
-            # driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
+            ## Test Environment
+            ## Select Appropriate URL based on the Environment Value (env)
+            baseURL = Environments.return_environments(env)
 
             # Maximize Window; Launch URL
-            # driver.maximize_window()
-            # driver.get(baseURL)
 
-            driver.get(base_URL)
+            driver.get(baseURL)
 
             driver.implicitly_wait(3)
 
@@ -305,8 +280,6 @@ class TestCreateQuote():
             time.sleep(3)
 
             # At this point, script is re-directed to service center login screen
-            # This works on DEV
-            # TODO: FIX redirection; should redirect back to Service Center
             saw_confirm_issue.click_return_to_Admin_Interface()
 
             time.sleep(2)
@@ -399,8 +372,6 @@ class TestCreateQuote():
 
             # Click Policy
             ap.click_policy_link(policy_text)
-
-            # Code works up to this point
 
             # Wait
             driver.implicitly_wait(3)
