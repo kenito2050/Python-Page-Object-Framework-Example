6597ccd6fd527be718d5369c0b2874884cd36234
Ken Villarruel <kvillarruel@nasinsurance.com>
2018-10-22 12:50:41 -0700

    Updated Script Script now uses Data Generator and Date Time Utility Classes

--- tests/products/LEXR_TENANT_Create_Policy.py
+++ tests/products/LEXR_TENANT_Create_Policy.py
@@ -1,7 +1,5 @@
 import datetime
-import os
 import time
-import unittest
 from urllib.parse import urlparse, parse_qs
 from xml.etree import ElementTree as ET
 
@@ -9,7 +7,6 @@ import xlrd
 from faker import address
 from faker import company
 from faker import name
-from selenium import webdriver
 
 from pages.producer_center.client_contact_page import ClientContact
 from pages.producer_center.client_search_page import ClientSearch
@@ -41,6 +38,8 @@ from pages.service_center.subjectivities import Subjectivities
 from utilities.Environments.Environments import Environments
 from utilities.state_capitals.state_capitals import StateCapitals
 from utilities.zip_codes_state_capitals.zip_codes import ZipCodes
+from utilities.Faker.Data_Generator import Data_Generator
+from utilities.Date_Time_Generator.Date_Time_Generator import Date_Time_Generator
 from config_globals import *
 
 class TestCreateQuote():
@@ -120,47 +119,33 @@ class TestCreateQuote():
             else:
                 break
 
-            ## Determine Test Environment to run scripts
+            # Create Instance of Data Generator
+            dg = Data_Generator()
 
-            ## Select Appropriate URL based on the Environment Value from above
-            base_URL = Environments.return_environments(env)
-
-            first_name = name.first_name()
-            last_name = name.last_name()
-            company_name = company.company_name()
-            # company_name_string = company_name
-            company_name_string = "QA Test" + " " + "-" + " " + "Dr." + " " + first_name + " " + last_name + " " + "dba" + " " + company_name
-            address_value = address.street_address()
+            # Create Company Name Value
+            company_name_string = dg.create_full_company_name()
+            # Create Street Address Value
+            address_value = dg.create_street_address()
             city = StateCapitals.return_state_capital(state)
             postal_code = ZipCodes.return_zip_codes(state)
+
+            # Create Instance of Date Time Generator
+            dtg = Date_Time_Generator()
+            # Create Today's Date
+            date_today = dtg.return_date_today()
 
             # Access XML to retrieve login credentials
             tree = ET.parse(str(config_file_directory / 'resources.xml'))
             login_credentials = tree.getroot()
-            username = (login_credentials[0][0].text)
+            username = (login_credentials[1][0].text)
             password = (login_credentials[1][1].text)
 
-            # No Code to Select Contract Class as it is not Selected
-
-            # Date Variables
-            date_today = time.strftime("%m/%d/%Y")
-            ad_hoc_effectiveDate = "09/06/2017"
-
-            # Convert effective_date value to format MM/DD/YYYY
-            d = xlrd.xldate_as_tuple(int(effective_date), 0)
-            # convert date tuple in mm-dd-yyyy format
-            d = datetime.datetime(*(d[0:3]))
-            effective_date_formatted = d.strftime("%m/%d/%Y")
-
-            # Initialize Driver; Launch URL
-            # baseURL = "https://svcdemo1.wn.nasinsurance.com/"
-            # driver = webdriver.Chrome(os.path.join(config_file_directory, 'chromedriver.exe'))
+            ## Test Environment
+            ## Select Appropriate URL based on the Environment Value (env)
+            baseURL = Environments.return_environments(env)
 
             # Maximize Window; Launch URL
-            # driver.maximize_window()
-            # driver.get(baseURL)
-
-            driver.get(base_URL)
+            driver.get(baseURL)
 
             driver.implicitly_wait(3)
 
@@ -182,10 +167,6 @@ class TestCreateQuote():
             cs.manually_input_new_client()
             cs.enter_new_client_name_address(company_name_string, address_value, city, state)
             cc = ClientContact(driver)
-
-            # TODO:
-            # Code now parses URL String & retrieves application ID
-            # cc.parse_url_get_app_id()
 
             # Get the Application ID from URL -- THIS WORKS
             current_url = driver.current_url
@@ -310,17 +291,9 @@ class TestCreateQuote():
             time.sleep(3)
 
             # At this point, script is re-directed to service center login screen
-            # This works on DEV
-            # TODO: FIX redirection; should redirect back to Service Center
             saw_confirm_issue.click_return_to_Admin_Interface()
 
             time.sleep(2)
-
-            # This section is necessary ONLY on STAGE
-            # Call Login methods from Pages.home.login_page.py
-            # lp = LoginPage(driver)
-            # lp.login(username, password)
-            # nb = NavigationBar(driver)
 
             # Click Applications link on Navigation Bar
             nb.click_applications()
@@ -329,10 +302,6 @@ class TestCreateQuote():
             app_page = ApplicationsPage(driver)
             app_page.enter_application_id(application_id)
             app_page.click_search_button()
-
-            # Click on application id link
-            # THIS IS NOT WORKING
-            # app_page.click_application_id_link(application_id)
 
             # Navigate to Application Details page
             new_current_url = driver.current_url
@@ -352,8 +321,6 @@ class TestCreateQuote():
             # Approve Subjectivities
             sub = Subjectivities(driver)
             sub.set_all_subjectivities_to_recieved()
-            # sub.change_open_subjectivities_to_received()
-            # sub.select_yes_to_subjectivities_met()
             sub.click_submit()
             sub.click_agent_link()
 
@@ -414,8 +381,6 @@ class TestCreateQuote():
 
             # Click Policy
             ap.click_policy_link(policy_text)
-
-            # Code works up to this point
 
             # Wait
             driver.implicitly_wait(3)
